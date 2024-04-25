c88726c60f09e866b6a216bbc44d20108eb1ac1e
Owen Taylor <48262631+owentechnologist@users.noreply.github.com>
2024-04-25 13:27:44 -0500

    update

--- spingtest.py
+++ spingtest.py
@@ -2,7 +2,7 @@ import redis
 
 #NB: sping is an expensive command server side 
 # the more general purpose = 'ping' is usually a better choice
-connection = redis.Redis( host='searchme.southcentralus.redisenterprise.cache.azure.net', port=10000,password='VakhcJeA8U4PLjA1uThXexC8ql98Ov2BtVvVj76KBfQ=',decode_responses=True)
+connection = redis.Redis( host='bob.cache.azure.net', port=10000,password='VakhcJeA8U4PLjA1uThXexC8ql98Ov2BtVvVj76KBfQot=',decode_responses=True)
 output_list = connection.execute_command('sping')
 for x in output_list:
     print(x)
